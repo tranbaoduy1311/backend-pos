f279b9bcd3fe316419e29a57e2f1770809c60050
Tran Bao Duy <dytran783@gmail.com>
2026-01-22 23:32:37 +0700

    Allow CORS for Vercel

--- src/main/java/com/nhahang/pos/pos_backend/controller/TableController.java
+++ src/main/java/com/nhahang/pos/pos_backend/controller/TableController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tables")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class TableController {
 
     @Autowired
